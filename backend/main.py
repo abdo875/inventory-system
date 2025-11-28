261e1d803d96be0aeeef2e84b7a5e696a4cb5e98
Waleed Amgad <waleedbookey@gmail.com>
2025-11-28 14:55:29 +0200

    added monitoring and changed some things in main and compose

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,8 @@
-from fastapi import FastAPI, Depends, Request, Form
+from fastapi import FastAPI, Depends, Request, Form, Response
 from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
 from sqlalchemy.orm import Session
+from prometheus_client import Counter, generate_latest, CONTENT_TYPE_LATEST
 
 from database import SessionLocal, engine, Base
 from models import Product
@@ -10,7 +11,15 @@ from models import Product
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+Request_Count=Counter("backend_requests_total","Total requests",["route"])
 templates = Jinja2Templates(directory="templates")
+
+#Promethus Functions
+#-------------------
+@app.get("/metrics")
+def metrics():
+    return Response(generate_latest(),media_type=CONTENT_TYPE_LATEST) 
+
 
 # Dependency for DB session
 def get_db():
@@ -26,6 +35,7 @@ def get_db():
 # --------------------------
 @app.get("/", response_class=HTMLResponse)
 def home(request: Request, db: Session = Depends(get_db)):
+    Request_Count.labels(route="/").inc() #taking metrics from here
     products = db.query(Product).all()
     return templates.TemplateResponse("index.html", {"request": request, "products": products})
 
@@ -58,3 +68,5 @@ def delete_product(
         db.delete(product)
         db.commit()
     return RedirectResponse(url="/", status_code=303)
+
+
